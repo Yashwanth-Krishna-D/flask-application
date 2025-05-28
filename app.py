1d2fcba077cdc149c41db08fbe4a52a6e773f8e3
Yashwanth-Krishna-D <yashwanth23110541@gmail.com>
2025-05-28 08:14:31 +0000

    Completed Trivial website skeleton

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+about_content = ["I'm a third-year B.Tech Computer Science student at Shiv Nadar University, specializing in Artificial Intelligence and Data Science, with a strong focus on Machine Learning, Deep Learning, and Generative AI. My interests lie in building intelligent systems and applying neural networks and large language models to solve real-world problems in areas like computer vision, NLP, and predictive modeling.",
+
+              "I enjoy diving deep into model architectures, fine-tuning pre-trained models, and exploring the latest developments in AI and GenAI. Currently, I’m seeking internship opportunities where I can contribute to impactful projects, collaborate with forward-thinking teams, and continue growing as an AI practitioner."]
+
+education_content = {
+  "University :": "Shiv Nadar University, Chennai",
+  "Degree :": "B.Tech in Artificial Intelligence and Data Science",
+  "Batch :": "2023-2027",
+  "Grade :": "9.59 CGPA"
+}
+
+project_content = []
+
+footer_content = "//This is a footer text.. modify them later"
+
 @app.route('/')
 def homePage():
-  return render_template("home.html")
+  return render_template("home.html", about = about_content, education = education_content , footer = footer_content)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug= True)
